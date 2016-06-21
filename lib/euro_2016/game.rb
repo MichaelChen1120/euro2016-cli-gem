ceaee0ecf659e6a17f52ab91c6c1862fdc76aec1
Michael Chen <michael.chen.1120@gmail.com>
2016-06-21 21:01:35 +0000

    Done.

--- lib/euro_2016/game.rb
+++ lib/euro_2016/game.rb
@@ -7,19 +7,6 @@ class Euro2016::Game
     binding.pry
   end
 
-  def get_page
-    Nokogiri::HTML(open("http://www.espnfc.us/european-championship/74/scores"))
-  end
+  
 
-  def scrape_games
-    doc=get_page.css("div.scores")
-    games=doc.search("div.score-content")
-    games.collect do |game|
-      game.css("a").attribute("href").text
-    end
-  end
-
-  def doc
-    @doc ||= Nokogiri::HTML(open(self.url))
-  end
 end
